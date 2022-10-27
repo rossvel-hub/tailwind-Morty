2f7697e7b5529f65bca776727a79c772423c1bee
Alma <ommigos@gmail.com>
2022-10-27 13:42:49 -0500

    instalaciones tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-9xl bg-black text-rose-300 font-bold underline p-5 border-4 border-yellow-300 rounded-full text-center">
+      Hello world!
+    </h1>
   );
 }
 
